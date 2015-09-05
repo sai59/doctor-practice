faac818b665cb2575a5ae5578a2f85963235fb2e
sai59 <saikrishnakota@hotmail.com>
2015-09-05 09:22:37 +0530

    Add sti support to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905031445) do
+ActiveRecord::Schema.define(version: 20150905035159) do
 
   create_table "appointments", force: :cascade do |t|
     t.date     "visit"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150905031445) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "role",                   limit: 255
+    t.string   "type",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
