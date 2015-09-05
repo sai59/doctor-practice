3f0b7cda2e95d48b0dbd54a159ab6729d5df244f
sai59 <saikrishnakota@hotmail.com>
2015-09-05 08:46:13 +0530

    Add doctoId to Appointment.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905030851) do
+ActiveRecord::Schema.define(version: 20150905031445) do
 
   create_table "appointments", force: :cascade do |t|
     t.date     "visit"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150905030851) do
     t.string   "reason",      limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "doctor_id",   limit: 4
   end
 
   create_table "doctors", force: :cascade do |t|
